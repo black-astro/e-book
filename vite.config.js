501ece8d3ce8bff58300895fd88a71737a4adced
black-astro <gntj3200@gmail.com>
2023-11-29 09:41:57 +0900

    git 배포

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,19 @@ import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
+// GitHub 사용자 이름과 리포지토리 이름에 맞게 수정하세요
+const base = process.env.NODE_ENV === 'production' ? '/black-astro/' : '/';
+
 export default defineConfig({  
+  base, //git base
   plugins: [
     react(),
     svgr(),
   ],
   esbuild: {
-    loader: {
-      '.js': 'jsx', // '.js' 파일을 'jsx'로 처리
-    },
+    // loader: {
+    //   '.js': 'jsx', // '.js' 파일을 'jsx'로 처리
+    // },
   },
   server: {
     port: 3031,
